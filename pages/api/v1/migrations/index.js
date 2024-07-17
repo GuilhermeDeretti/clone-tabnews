a18ea920a0c451a84e4a86c763973328824b2add
Guitti <guilhermederetti9@gmail.com>
2024-07-17 23:39:02 +0100

    added test for invalid connection method/ fixed the way migrations deal with invalid connection methods

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -12,27 +12,34 @@ export default async function migrations(request, response) {
     verbose: "true",
     migrationsTable: "pgmigrations",    
   }
-  if (request.method === "GET") {
-    const pendingMigrations = await migrationRunner(defaultMigrationOptions);  
-    await dbClient.end();
-    return response.status(200).json(pendingMigrations);
-  }
 
-  if (request.method === "POST") {
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      dryRun: false,   
-    });  
+  try {  
 
-    await dbClient.end();
-
-    if(migratedMigrations.length > 0){
-    return response.status(201).json(migratedMigrations);    
+    if (request.method === "GET") {
+      const pendingMigrations = await migrationRunner(defaultMigrationOptions);        
+      response.status(200).json(pendingMigrations);
     }
 
-    return response.status(200).json(migratedMigrations);    
-  }
+    if (request.method === "POST") {
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationOptions,
+        dryRun: false,   
+      });  
 
-  return response.status(405).end();
+      if(migratedMigrations.length > 0){
+      response.status(201).json(migratedMigrations);    
+      }
+
+      response.status(200).json(migratedMigrations);    
+    }
+
+    return response.status(405).end();
+
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await dbClient.end();
+    return response;
+  }
   //add try catch in the future
 }
--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -6,7 +6,7 @@ async function cleanDatabase() {
   await database.query("drop schema public cascade; create schema public;")
 }
 
-test("POST to /api/v1/migrations should return 200", async () => {
+test("POST to /api/v1/migrations should return 201", async () => {
   const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
     method: 'POST'});
   expect(response1.status).toBe(201);
@@ -21,10 +21,17 @@ test("POST to /api/v1/migrations should return 200", async () => {
   const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
     method: 'POST'});
   expect(response2.status).toBe(200);
-
+  console.log(response2.statusText);
   const response2Body = await response2.json();
 
   expect(Array.isArray(response2Body)).toBe(true);
   expect(response2Body.length).toBe(0);
   //response zero as they have been executed before, and should not be executed again
+});
+
+test("POST to /api/v1/migrations should return 405", async () => {
+  const response3 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: 'PATCH'});
+  expect(response3.status).toBe(405);
+  expect(response3.statusText).toBe('Method Not Allowed');
 });
